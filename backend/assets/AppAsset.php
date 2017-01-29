a4b9ad715fdf4aa198288af0839903e92c8842ef
pangpond <pondzz@gmail.com>
2017-01-29 16:45:25 +0700

    add themes adminLTE to backend

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -3,21 +3,37 @@
 namespace backend\assets;
 
 use yii\web\AssetBundle;
+use Yii;
+
+// set @themes alias so we do not have to update baseUrl every time we change themes
+Yii::setAlias('@themes', Yii::$app->view->theme->baseUrl);
 
 /**
  * Main backend application asset bundle.
  */
 class AppAsset extends AssetBundle
 {
+    // public $basePath = '@webroot';
+    // public $baseUrl = '@web';
+
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $baseUrl = '@themes';
+
     public $css = [
-        'css/site.css',
+        'css/AdminLTE.css',
+        'css/skins/skin-red.min.css'
     ];
+
+    public $cssOptions = ['type'=>'text/css'];  
+
     public $js = [
+        'js/app.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
+        'rmrevin\yii\ionicon\AssetBundle',
     ];
 }
--- backend/config/main.php
+++ backend/config/main.php
@@ -37,6 +37,18 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'view'=>[
+            'theme'=>[
+                'basePath' => '@app/themes/adminlte',
+                'baseUrl' => '@web/themes/adminlte',
+                'pathMap' => [
+                    '@app/views' => '@app/themes/adminlte',
+                ],
+            ]
+        ],
+        'assetManager' => [
+            'appendTimestamp' => true,
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
