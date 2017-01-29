7bc6d7a1b25be27a885a5584738631293ad0f454
pangpond <pondzz@gmail.com>
2017-01-29 15:38:42 +0700

    init project

---
+++ common/config/main.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            // 'class' => 'yii\caching\FileCache',
+          'class' => 'yii\caching\MemCache',
+            'servers' => [
+              [
+                'host' => 'memcached',
+                'port' => 11211,
+                'weight' => 100,
+              ]
+            ],
+        ],
+    ],
+];
